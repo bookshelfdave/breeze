9a21826eac4049c2105ea29e874b64856a23ca8e
Dave Parfitt <dparfitt@basho.com>
2014-01-30 19:35:22 -0500

    working on unit testing visitors

--- src/main/java/com/metadave/breeze/Breeze.java
+++ src/main/java/com/metadave/breeze/Breeze.java
@@ -1,6 +1,7 @@
 package com.metadave.breeze;
 
 import com.metadave.breeze.ast.BreezeASD;
+import com.metadave.breeze.ast.BreezeASTVisitor;
 import com.metadave.breeze.ast.BreezeVerifyVisitor;
 import com.metadave.breeze.parser.BreezeLexer;
 import com.metadave.breeze.parser.BreezeParser;
@@ -10,8 +11,10 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.util.List;
+
 public class Breeze {
-    public static void parse(String s) throws Exception {
+    public static void parse(String s, List<BreezeASTVisitor> visitors) throws Exception {
         ANTLRInputStream input = new ANTLRInputStream(s);
         BreezeLexer lexer = new BreezeLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -26,10 +29,18 @@ public class Breeze {
             BreezeASD asd = (BreezeASD)b.getValue(ctx);
             System.out.println(asd);
             asd.accept(new BreezeVerifyVisitor());
-            //asd.accept(new BreezeSTRender());
+            if(visitors != null) {
+                for(BreezeASTVisitor v: visitors) {
+                    asd.accept(v);
+                }
+            }
         } catch (Throwable e) {
             //e.printStackTrace ();
             throw new Exception("Breeze parse error", e);
         }
+    }
+
+    public static void parse(String s) throws Exception {
+         parse(s, null);
     }
 }
--- src/test/java/com/metadave/TestBreeze.java
+++ src/test/java/com/metadave/TestBreeze.java
@@ -1,7 +1,13 @@
 package com.metadave;
 
 import com.metadave.breeze.Breeze;
+import com.metadave.breeze.ast.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -101,4 +107,103 @@ public class TestBreeze {
         }
     }
 
+    @Test
+    public void testDefDups2() {
+        try {
+            String s =
+                    "stm = Compound(stm,stm)\n" +
+                            "| Assign(identifier, exp)\n" +
+                            "| Print(exp_list)\n" +
+                            "exp_list = ExpList(exp, exp_list) | Nil\n" +
+                            "exp = Id(identifier)\n" +
+                            "| Num(int)\n" +
+                            "| Op(exp, binop, exp)\n" +
+                            "binop = Plus | Minus | Times | Times";
+            Breeze.parse(s);
+            fail();
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+
+
+    @Test
+    public void testVisitor() {
+        try {
+            TestVisitor v = new TestVisitor();
+
+            String s =
+                    "stm = Compound(stm,stm)\n" +
+                            "| Assign(identifier, exp)\n" +
+                            "| Print(exp_list)\n" +
+                    "foo = (sum, sum)\n" +
+                    "exp_list = ExpList(exp, exp_list) | Nil\n" +
+                    "exp = Id(identifier)\n" +
+                            "| Num(int)\n" +
+                            "| Op(exp, binop, exp)\n" +
+                    "binop = Plus | Minus | Times | Div";
+
+            List<BreezeASTVisitor> visitors = new ArrayList<BreezeASTVisitor>();
+            visitors.add(v);
+            Breeze.parse(s, visitors);
+            assertTrue(v.objs.contains(com.metadave.breeze.ast.BreezeSumType.class));
+            assertTrue(v.objs.contains(com.metadave.breeze.ast.BreezeProductType.class));
+            assertTrue(v.objs.contains(com.metadave.breeze.ast.BreezeFields.class));
+            assertTrue(v.objs.contains(com.metadave.breeze.ast.BreezeField.class));
+            assertTrue(v.objs.contains(com.metadave.breeze.ast.BreezeDefinition.class));
+            assertTrue(v.objs.contains(com.metadave.breeze.ast.BreezeConstructor.class));
+            assertTrue(v.objs.contains(com.metadave.breeze.ast.BreezeASD.class));
+
+            assertEquals(7, v.objs.size());
+        } catch (Exception e) {
+            e.printStackTrace();;
+            fail();
+        }
+    }
+}
+
+class TestVisitor implements BreezeASTVisitor {
+    public Set<Object> objs = new HashSet<Object>();
+
+    @Override
+    public void visit(BreezeASD asd) throws Exception {
+        objs.add(asd.getClass());
+    }
+
+    @Override
+    public void visit(BreezeConstructor ctor) throws Exception {
+        objs.add(ctor.getClass());
+        //assertEquals("XXX", ctor.getId());
+    }
+
+    @Override
+    public void visit(BreezeDefinition def) throws Exception {
+        objs.add(def.getClass());
+    }
+
+    @Override
+    public void visit(BreezeField field) throws Exception {
+        objs.add(field.getClass());
+    }
+
+    @Override
+    public void visit(BreezeFields fields) throws Exception {
+        objs.add(fields.getClass());
+    }
+
+    @Override
+    public void visit(BreezeProductType pt) throws Exception {
+        objs.add(pt.getClass());
+    }
+
+    @Override
+    public void visit(BreezeSumType st) throws Exception {
+        objs.add(st.getClass());
+    }
+
+    @Override
+    public void visit(BreezeType t) throws Exception {
+        // it's a trap! ProductType + SumType already have this case covered
+        objs.add(t.getClass());
+    }
 }
